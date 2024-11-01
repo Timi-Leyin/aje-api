a76c12b5d332bd8530ddfd749a358957f0ed183a
OriginalTimi <timileyinoyelekan11@gmail.com>
2024-11-01 18:50:11 +0000

    fix: added limit to pagination

--- src/helpers/paginition.ts
+++ src/helpers/paginition.ts
@@ -1,8 +1,10 @@
 import { Request } from "express";
+import { DATA_LIMIT } from "../constants";
 
 export const getPaginaionParams = (req: Request) => {
   const page = parseInt(req.query.page as string) || 1;
-  const limit = parseInt(req.query.limit as string) || 10;
+  const reqLimit = parseInt(req.query.limit as string) || DATA_LIMIT;
+  const limit = Math.min(reqLimit, DATA_LIMIT);
 
   const offset = (page - 1) * limit;
 
