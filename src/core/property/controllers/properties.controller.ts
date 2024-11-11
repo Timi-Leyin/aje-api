d38a5cf76127c51d0662e53b2b3de2d7aa743fb3
Original Timi <timileyinoyelekan11@gmail.com>
2024-11-11 14:56:06 +0100

    feat: search properties

--- src/core/property/controllers/properties.controller.ts
+++ src/core/property/controllers/properties.controller.ts
@@ -3,14 +3,26 @@ import errorHandler from "../../../helpers/error-handler";
 import { getPaginaionParams } from "../../../helpers/paginition";
 import propertyService from "../property.service";
 import responseObject from "../../../helpers/response-object";
+import { PRODUCT_TYPE } from "@prisma/client";
+
+type QueryParams = {
+  query: string;
+  type: PRODUCT_TYPE;
+};
 
 export default async (req: Request, res: Response) => {
   try {
+    const { query, type } = req.query as QueryParams;
     const { limit, offset, page } = getPaginaionParams(req);
     const properties = await propertyService.getProperties({
       limit,
       offset,
       page,
+
+      where: {
+        title: query,
+        type,
+      },
     });
 
     return res.status(200).json(
