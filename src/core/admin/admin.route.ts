4eb2a5910a936829a2ccb8da378b6f24c45fe437
Original Timi <timileyinoyelekan11@gmail.com>
2024-12-22 13:33:25 +0100

    feat: send email

--- src/core/admin/admin.route.ts
+++ src/core/admin/admin.route.ts
@@ -5,14 +5,40 @@ import authGuard from "../../middlewares/auth-guard";
 import isAdmin from "../../middlewares/is-admin";
 import adminGetUsersController from "./controllers/admin-get-users-controller";
 import adminVerifyUser from "./controllers/adminVerifyUser";
-import { verifyUserSchema } from "../../schemas/admin.schema";
+import { sendEmailSchema, verifyUserSchema } from "../../schemas/admin.schema";
 import bodyValidation from "../../middlewares/body-validation";
+import sendMessages from "./controllers/send-messages";
+import { loginSchema } from "../../schemas/auth.schema";
+import createAd from "./controllers/create-ad";
 
 const adminRoutes = Router();
 
-adminRoutes.post("/login", adminLoginController);
+adminRoutes.post("/login", loginSchema, bodyValidation, adminLoginController);
 adminRoutes.get("/users", authGuard, isAdmin, adminGetUsersController);
 adminRoutes.get("/", authGuard, isAdmin, adminProfileController);
-adminRoutes.post("/verification", authGuard, isAdmin,verifyUserSchema, bodyValidation, adminVerifyUser);
+adminRoutes.post(
+  "/verification",
+  authGuard,
+  isAdmin,
+  verifyUserSchema,
+  bodyValidation,
+  adminVerifyUser
+);
+adminRoutes.post(
+  "/send-message",
+  authGuard,
+  isAdmin,
+  sendEmailSchema,
+  bodyValidation,
+  sendMessages
+);
+adminRoutes.post(
+  "/ads",
+  authGuard,
+  isAdmin,
+  sendEmailSchema,
+  bodyValidation,
+  createAd
+);
 
 export default adminRoutes;
--- src/schemas/admin.schema.ts
+++ src/schemas/admin.schema.ts
@@ -1,10 +1,25 @@
 import { body } from "express-validator";
 
-export const verifyUserSchema= [
-      body("userId")
-        .trim()
-        .isString()
-        .withMessage("userId is Required")
-        .isLength({ min: 3 })
-        .withMessage("Invalid userId"),
-]
+export const verifyUserSchema = [
+  body("userId")
+    .trim()
+    .isString()
+    .withMessage("userId is Required")
+    .isLength({ min: 3 })
+    .withMessage("Invalid userId"),
+];
+export const sendEmailSchema = [
+  body("message")
+    .trim()
+    .isString()
+    .withMessage("message is Required")
+    .isLength({ min: 3 })
+    .withMessage("message must be more 3"),
+
+  body("subject")
+    .trim()
+    .isString()
+    .withMessage("subject is Required")
+    .isLength({ min: 3 })
+    .withMessage("subject must be more 3"),
+];
