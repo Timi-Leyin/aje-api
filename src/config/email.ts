2950aea6e7ecc74fcc0d9e8fdf732bafaf0273a1
Original Timi <timileyinoyelekan11@gmail.com>
2024-12-22 20:37:40 +0100

    email

--- src/config/email.ts
+++ src/config/email.ts
@@ -2,9 +2,7 @@ import nodemailer from "nodemailer";
 import { ENV } from "../constants/env";
 
 export const emailTransporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: ENV.EMAIL_USER,
     pass: ENV.EMAIL_PASS,
