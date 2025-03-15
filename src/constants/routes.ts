117e5576d91149b09dd7d2f9d6863970830cf1c0
Timi-Leyin <timileyinoyelekan11@gmail.com>
2025-03-15 13:38:52 +0100

    feat: confirm email

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -32,6 +32,8 @@ export const ROUTES = {
   LOGIN: "/login",
   REGISTER: "/register",
   CHECK_EMAIL: "/check-email",
+  SEND_OTP: "/send-otp-email",
+  VERIFY_OTP: "/verify-otp-email",
   GOOGLE_AUTH: "/google",
   GOOGLE_AUTH_CALLBACK: "/google/callback",
   GOOGLE_AUTH_CALLBACK_SUCCESS: "/google/callback/success",
--- src/core/auth/auth.route.ts
+++ src/core/auth/auth.route.ts
@@ -5,6 +5,8 @@ import {
   forgottenPasswordSchema,
   loginSchema,
   registerSchema,
+  verifyEmailOtpSchema,
+  verifyEmailSchema,
   verifyOtpSchema,
 } from "../../schemas/auth.schema";
 import bodyValidation from "../../middlewares/body-validation";
@@ -21,6 +23,8 @@ import googleFailureController from "./controllers/google-failure.controller";
 import { multerUpload } from "../../config/multer";
 import { ENV } from "../../constants/env";
 import checkEmailController from "./controllers/check-email.controller";
+import sendOtpController from "./controllers/send-otp.controller";
+import verifyEmailOtpController from "./controllers/verify-email-otp.controller";
 
 const authRoute = Router();
 
@@ -49,12 +53,27 @@ const registerationUpload = multerUpload.fields([
 ]);
 // registration
 authRoute.get(ROUTES.CHECK_EMAIL, checkEmailController);
+
 authRoute.post(
   ROUTES.REGISTER,
   registerationUpload,
   registerSchema,
   bodyValidation,
   registerController
+);
+
+authRoute.post(
+  ROUTES.SEND_OTP,
+  verifyEmailSchema,
+  bodyValidation,
+  sendOtpController
+);
+
+authRoute.post(
+  ROUTES.VERIFY_OTP,
+  verifyEmailOtpSchema,
+  bodyValidation,
+  verifyEmailOtpController
 );
 
 authRoute.post(
